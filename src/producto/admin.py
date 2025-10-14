3853cedf73002439df1e13eea12d4475a204691d
Esteban <esteban.horacio.acevedo@gmail.com>
2025-10-14 20:39:25 -0300

    Registra el modelo Producto en el panel de administración

--- src/producto/admin.py
+++ src/producto/admin.py
@@ -7,3 +7,13 @@ from . import models
 class CategoriaAdmin(admin.ModelAdmin):
     list_display = ("nombre", "descripcion")
     search_fields = ("nombre",)
+
+
+@admin.register(models.Producto)
+class ProductoAdmin(admin.ModelAdmin):
+    list_display = ("categoria", "nombre", "precio", "stock", "disponible")
+
+    list_display_links = ("nombre",)
+    list_filter = ("disponible", "categoria")
+    list_editable = ("disponible",)
+    search_fields = ("nombre", "descripcion")
