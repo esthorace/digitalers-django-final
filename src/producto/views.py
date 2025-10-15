8749ac53c666939281990a601bc7b7bb87c7e2f0
Esteban <esteban.horacio.acevedo@gmail.com>
2025-10-14 21:37:49 -0300

    Crea la vista y plantilla para la lista de categorías de productos

--- src/producto/views.py
+++ src/producto/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
 
+from . import models
+
 
 def index(request):
     return render(request, "producto/index.html")
+
+
+def categoria_list(request):
+    queryset = models.Categoria.objects.all()
+    context = {"object_list": queryset}
+    return render(request, "producto/categoria_list.html", context)
