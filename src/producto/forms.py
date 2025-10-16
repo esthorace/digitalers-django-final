1c22c90fcdd00a23ec621dddb74f47e783d17435
Esteban <esteban.horacio.acevedo@gmail.com>
2025-10-16 19:36:51 -0300

    Añade validación al campo 'nombre' en form
    Añade validación al campo 'nombre' en el formulario de categoría
    para restringir caracteres y longitud

--- src/producto/forms.py
+++ src/producto/forms.py
@@ -1,3 +1,5 @@
+import re
+
 from django import forms
 
 from . import models
@@ -8,3 +10,22 @@ class CategoriaForm(forms.ModelForm):
         model = models.Categoria
         # fields = ["nombre", "descripcion"]
         fields = "__all__"
+
+    def clean_nombre(self):
+        # Expresión regular
+        patron = r"^[a-zA-ZáéíóúÁÉÍÓÚüÜñÑ\s\-\(\)]+$"
+
+        nombre: str = self.cleaned_data.get("nombre", "")
+        nombre = nombre.strip()
+
+        if not re.fullmatch(patron, nombre):
+            raise forms.ValidationError(
+                "Solo se permiten letras, espacios, paréntesis, acentos,"
+                " diéresis y guiones medios. No se admiten números ni caracteres especiales."
+            )
+
+        if len(nombre) < 3 or len(nombre) > 50:
+            raise forms.ValidationError(
+                "Longitud mínima de 3 caracteres y máxima de 50"
+            )
+        return nombre
