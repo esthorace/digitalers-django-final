655f35af2127e1c96552ae564155754957367517
Esteban <esteban.horacio.acevedo@gmail.com>
2025-10-23 19:48:34 -0300

    Añade formulario de registro de usuario

--- src/core/urls.py
+++ src/core/urls.py
@@ -1,12 +1,13 @@
 from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 
-from . import views
+from .views import Register, index
 
 app_name = "core"
 
 urlpatterns = [
-    path("", views.index, name="index"),
+    path("", index, name="index"),
     path("login/", LoginView.as_view(template_name="core/login.html"), name="login"),
     path("logout/", LogoutView.as_view(template_name="core/logout.html"), name="logout"),
+    path("register/", Register.as_view(template_name="core/register.html"), name="register"),
 ]
--- src/core/views.py
+++ src/core/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+
+from .forms import CustomUserCreationForm
 
 
 def index(request):
     return render(request, "core/index.html")
+
+
+class Register(CreateView):
+    form_class = CustomUserCreationForm
+    success_url = reverse_lazy("core:login")
