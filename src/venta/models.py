cecc10524b800e1d92e359a3e8dd47197df6601d
Esteban <esteban.horacio.acevedo@gmail.com>
2025-10-23 20:36:48 -0300

    Añade modelos Vendedor y Venta

--- src/venta/models.py
+++ src/venta/models.py
@@ -1,3 +1,32 @@
+from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Vendedor(models.Model):
+    usuario = models.OneToOneField(User, on_delete=models.CASCADE, related_name="vendendor")
+    celular = models.CharField(max_length=50)
+    avatar = models.ImageField(upload_to="avatares", blank=True, null=True)
+
+    def __str__(self):
+        return self.usuario.get_full_name()
+
+
+class Venta(models.Model):
+    vendedor = models.ForeignKey(Vendedor, on_delete=models.DO_NOTHING)
+    producto = models.ForeignKey("producto.Producto", on_delete=models.DO_NOTHING)
+    cantidad = models.PositiveIntegerField()
+    precio_total = models.DecimalField(max_digits=10, decimal_places=2, editable=False)
+    fecha_venta = models.DateTimeField(default=timezone.now, editable=False)
+
+    class Meta:
+        ordering = ("-fecha_venta",)
+
+    def clean(self):
+        if self.cantidad > self.producto.stock:
+            raise ValidationError("La cantidad a vender no puede ser mayo al stock disponible")
+
+    def save(self, *args, **kwargs):
+        self.precio_total = self.producto.precio * self.cantidad
+        super().save(*args, **kwargs)
